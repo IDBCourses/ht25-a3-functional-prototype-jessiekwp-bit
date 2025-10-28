5fb282e8271fa4daa623780cdccc6a1de415c995
jessiekwp-bit <jessiekwp@gmail.com>
2025-10-28 15:56:24 +0100

    added array and try to modify thing1 but failed

--- Template/script.js
+++ Template/script.js
@@ -6,13 +6,21 @@
 import * as Util from "./util.js";
 
 // State variables are the parts of your program that change over time.
-
+let things = [Util.thing, Util.createThing];
 
 // Settings variables should contain all of the "fixed" parts of your programs
 
 
 // Code that runs over and over again
 function loop() {
+Util.setPositionPixels(window.innerWidth/2, window.innerHeight/2, things[0]);
+Util.setColour(270,100,80,0.5, things[0]);
+Util.setRoundedness(0);
+Util.setSize(100);
+Util.setRotation(135, things[0]);
+
+Util.createThing()
+Util.setColour(270,100,80,0.5, things[1]);
 
 
   window.requestAnimationFrame(loop);
@@ -21,13 +29,10 @@ function loop() {
 // Setup is run once, at the start of the program. It sets everything up for us!
 function setup() {
   // Put your event listener code here
-Util.setPositionPixels(window.innerWidth/2, window.innerHeight/2);
-Util.setColour(270,100,80,0.5);
-Util.setRoundedness(0.25);
-Util.setSize(100);
-Util.setRotation(135)
 
-Util.createThing()
+
+
+
 
   window.requestAnimationFrame(loop);
 }
