f5612d63b12388d28b85e4cc4205058e25d91f64
jessiekwp-bit <jessiekwp@gmail.com>
2025-10-29 00:39:49 +0100

    created 2 more things and set dark diamond properties

--- Template/script.js
+++ Template/script.js
@@ -6,21 +6,27 @@
 import * as Util from "./util.js";
 
 // State variables are the parts of your program that change over time.
-let things = [Util.thing, Util.createThing];
+let things = [Util.thing, Util.createThing(), Util.createThing()];
+
 
 // Settings variables should contain all of the "fixed" parts of your programs
-
-
-// Code that runs over and over again
-function loop() {
-Util.setPositionPixels(window.innerWidth/2, window.innerHeight/2, things[0]);
-Util.setColour(270,100,80,0.5, things[0]);
+things[0] = Util.thing;
+Util.setPositionPixels(window.innerWidth*0.5, window.innerHeight*0.5, things[0]);
+Util.setColour(1,100,50,1, things[0]);
 Util.setRoundedness(0);
 Util.setSize(100);
 Util.setRotation(135, things[0]);
 
-Util.createThing()
-Util.setColour(270,100,80,0.5, things[1]);
+// Code that runs over and over again
+function loop() {
+
+things[1] = Util.createThing()
+Util.setPositionPixels(window.innerWidth*0.1, window.innerHeight*0.1, things[1]);
+Util.setColour(1,100,30,1, things[1]);
+Util.setRoundedness(0, things[1]);
+Util.setSize(100, things[1]);
+Util.setRotation(135, things[1]);
+
 
 
   window.requestAnimationFrame(loop);
@@ -29,6 +35,9 @@ Util.setColour(270,100,80,0.5, things[1]);
 // Setup is run once, at the start of the program. It sets everything up for us!
 function setup() {
   // Put your event listener code here
+things[0] = Util.thing;
+things[1] = Util.createThing()
+things[2] = Util.createThing()
 
 
 
