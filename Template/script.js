6dbfc0248a2abf0c0e583b6ea84e4bd4caff1976
jessiekwp-bit <jessiekwp@gmail.com>
2025-10-29 15:06:46 +0100

    darrk ruby goes down

--- Template/script.js
+++ Template/script.js
@@ -7,10 +7,13 @@ import * as Util from "./util.js";
 
 // State variables are the parts of your program that change over time.
 let things = [Util.thing, Util.createThing(), Util.createThing()];
-//const thing1x = 43290;
+let x = (window.innerWidth*0.2);
+let y = (window.innerHeight*0.2);
 
 
 // Settings variables should contain all of the "fixed" parts of your programs
+//const thing1x = 43290;
+
 //things[0] = Util.thing;
 Util.setPositionPixels(window.innerWidth*0.5, window.innerHeight*0.5, things[0]);
 Util.setColour(1,100,50,1, things[0]);
@@ -22,11 +25,10 @@ Util.setRotation(135, things[0]);
 //things[1] = DarkRuby
 //things[2] = LightRuby
 
-
 // Code that runs over and over again
 function loop() {
 
-Util.setPositionPixels(window.innerWidth*0.2, window.innerHeight*0.2, things[1]);
+Util.setPositionPixels(x, y, things[1]);
 Util.setColour(1,100,35,1, things[1]);
 Util.setRoundedness(0, things[1]);
 Util.setSize(100, things[1]);
@@ -39,27 +41,25 @@ Util.setSize(100, things[2]);
 Util.setRotation(135, things[2]);
 
 
-Util.setPosition(position, things[1]);
-
-Util.setPosition(position, things[2]);
-
-
-function updateXY(){
-  x = event.x / window.innerWidth;
-  y = event.y / window.innerHeight;
-}
-
   window.requestAnimationFrame(loop);
 }
 
-function DarkRubyGoesUp(){
-  y.position, things[1] -= 1;
-  //timeoutID = setTimeout(grow, 10);
+function DarkRubyGoesDown(){
+  Util.setPositionPixels(x, y +=10, things[1]);
 }
+
+
 
 // Setup is run once, at the start of the program. It sets everything up for us!
 function setup() {
   // Put your event listener code here
+
+  document.addEventListener('keydown', (event) => {
+    if(event.code === 'KeyC'){
+      console.log(`Key Down: Code ${event.code} | Key ${event.key}`);
+      DarkRubyGoesDown();
+    }
+  })
  
 
 
