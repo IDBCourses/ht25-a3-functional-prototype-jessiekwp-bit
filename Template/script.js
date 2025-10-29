2317035e96005aded26d2ac96e8d0ebf90a8964c
jessiekwp-bit <jessiekwp@gmail.com>
2025-10-29 11:03:48 +0100

    set 3 diamonds

--- Template/script.js
+++ Template/script.js
@@ -7,10 +7,11 @@ import * as Util from "./util.js";
 
 // State variables are the parts of your program that change over time.
 let things = [Util.thing, Util.createThing(), Util.createThing()];
+//const thing1x = 43290;
 
 
 // Settings variables should contain all of the "fixed" parts of your programs
-things[0] = Util.thing;
+//things[0] = Util.thing;
 Util.setPositionPixels(window.innerWidth*0.5, window.innerHeight*0.5, things[0]);
 Util.setColour(1,100,50,1, things[0]);
 Util.setRoundedness(0);
@@ -20,14 +21,17 @@ Util.setRotation(135, things[0]);
 // Code that runs over and over again
 function loop() {
 
-things[1] = Util.createThing()
-Util.setPositionPixels(window.innerWidth*0.1, window.innerHeight*0.1, things[1]);
-Util.setColour(1,100,30,1, things[1]);
+Util.setPositionPixels(window.innerWidth*0.2, window.innerHeight*0.2, things[1]);
+Util.setColour(1,100,35,1, things[1]);
 Util.setRoundedness(0, things[1]);
 Util.setSize(100, things[1]);
 Util.setRotation(135, things[1]);
 
-
+Util.setPositionPixels(window.innerWidth*0.8, window.innerHeight*0.8, things[2]);
+Util.setColour(1,100,75,1, things[2]);
+Util.setRoundedness(0, things[2]);
+Util.setSize(100, things[2]);
+Util.setRotation(135, things[2]);
 
   window.requestAnimationFrame(loop);
 }
@@ -35,9 +39,6 @@ Util.setRotation(135, things[1]);
 // Setup is run once, at the start of the program. It sets everything up for us!
 function setup() {
   // Put your event listener code here
-things[0] = Util.thing;
-things[1] = Util.createThing()
-things[2] = Util.createThing()
 
 
 
