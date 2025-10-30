14f8f2e32120cfd079bce402f13b346c774e3d45
jessiekwp-bit <jessiekwp@gmail.com>
2025-10-30 16:42:37 +0100

    dark ruby falling slowly

--- Template/script.js
+++ Template/script.js
@@ -8,11 +8,12 @@ import * as Util from "./util.js";
 // State variables are the parts of your program that change over time.
 let things = [Util.thing, Util.createThing(), Util.createThing()];
 let x = (window.innerWidth*0.2);
-let y = (window.innerHeight*0.2);
+let y = (window.innerHeight*0);
 let x2 = (window.innerWidth*0.8);
 let y2 = (window.innerHeight*0.8);
-let hue = 0
+//let hue = 0
 Util.setColour(0,100,30,1, things[1]);
+let gameOver = false; 
 
 
 // Settings variables should contain all of the "fixed" parts of your programs
@@ -43,6 +44,12 @@ Util.setRoundedness(0, things[2]);
 Util.setSize(100, things[2]);
 Util.setRotation(135, things[2]);
 
+if (y > window.innerHeight){
+  gameOver = true
+} else {
+  y +=0.5;
+}
+
 
   window.requestAnimationFrame(loop);
 }
@@ -52,7 +59,7 @@ function darkRubyGoesDown(){
 }
 
 function darkRubyGoesUp(){
-  Util.setPositionPixels(x, y -=5, things[1]);
+  Util.setPositionPixels(x, y -=20, things[1]);
 }
 
 function darkRubyGoesLeft(){
@@ -82,6 +89,9 @@ function lightRubyGoesRight(){
 function darkRubyChangesColour(){ 
   Util.setColour(hue += 40,100,30 ,1, things[1]); 
 } 
+
+
+
 
 // Setup is run once, at the start of the program. It sets everything up for us!
 function setup() {
