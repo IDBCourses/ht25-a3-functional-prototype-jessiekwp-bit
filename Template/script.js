251efcb3c8c186fd67294bdfe357dfa03b789296
jessiekwp-bit <jessiekwp@gmail.com>
2025-10-31 04:08:04 +0100

    changing to orange catcher

--- Template/script.js
+++ Template/script.js
@@ -8,11 +8,9 @@ import * as Util from "./util.js";
 // State variables are the parts of your program that change over time.
 let things = [Util.thing, Util.createThing(), Util.createThing()];
 let x = (window.innerWidth*0.2);
-let y = (window.innerHeight*0.2);
+let y = (window.innerHeight*0);
 let x2 = (window.innerWidth*0.8);
 let y2 = (window.innerHeight*0.8);
-//let hue = 0
-Util.setColour(0,100,30,1, things[1]);
 let gameOver = false; 
 
 
@@ -24,25 +22,25 @@ Util.setPositionPixels(window.innerWidth*0.5, window.innerHeight*0.5, things[0])
 Util.setColour(0,100,50,1, things[0]);
 Util.setRoundedness(0);
 Util.setSize(100);
-Util.setRotation(135, things[0]);
+Util.setRotation(0, things[0]);
 
 
 // Code that runs over and over again
 function loop() {
 
-//things[1] = darkRuby
+//things[1]
 Util.setPositionPixels(x, y, things[1]);
-
-Util.setRoundedness(0, things[1]);
+Util.setRoundedness(1, things[1]);
+Util.setColour(30,100,50,1, things[1]);
 Util.setSize(100, things[1]);
-Util.setRotation(135, things[1]);
+Util.setRotation(0, things[1]);
 
-//things[2] = lightRuby
+//things[2]
 Util.setPositionPixels(x2, y2, things[2]);
 Util.setColour(0,100,70,1, things[2]);
-Util.setRoundedness(0, things[2]);
+Util.setRoundedness(1, things[2]);
 Util.setSize(100, things[2]);
-Util.setRotation(135, things[2]);
+Util.setRotation(0, things[2]);
 
 if (y > window.innerHeight){
   gameOver = true
