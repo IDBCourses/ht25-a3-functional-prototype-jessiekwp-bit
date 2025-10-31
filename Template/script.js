17a0c533842ae5c0931c8ca4d26eec877d87da47
jessiekwp-bit <jessiekwp@gmail.com>
2025-11-01 00:18:02 +0100

    added const variable

--- Template/script.js
+++ Template/script.js
@@ -3,6 +3,11 @@
  *
  */
 
+//Game: "Orange Catcher"
+//Designer: Wan Pui Kwok
+//Rules: 
+
+
 import * as Util from "./util.js";
 
 // State variables are the parts of your program that change over time.
@@ -13,15 +18,11 @@ let px = (window.innerWidth*0.5);
 
 let oranges = [
   {x: 0.2, y:0},
-  {x: 0.5, y:0},
-  {x: 0.7, y:0},
-  {x: 0.3, y:0},
   {x: 0.9, y:0},
+  {x: 0.5, y:0},
+  {x: 0.3, y:0},
+  {x: 0.7, y:0},
 ];
-
-for(let i=0; i <5; i++){  //for loop 
-  oranges.push({x: Math.random(), y:0});
-}
 
 let currentOrangeIndex = 0; 
 let currentOrange = oranges[currentOrangeIndex];
@@ -32,7 +33,11 @@ let fallSpeed = 4.5;
 
 
 // Settings variables should contain all of the "fixed" parts of your programs
+const numRandomOranges = 5;
 
+for(let i=0; i < numRandomOranges ; i++){  //for loop 
+  oranges.push({x: Math.random(), y:0});
+}
 
 // Code that runs over and over again
 function loop() {
@@ -79,7 +84,6 @@ Util.setRoundedness(1, orange);
 Util.setSize(100, 100, orange);
 Util.setPositionPixels(x, y, orange);
 
-//setInterval(addOrange, 4000); // add an orange every 4 seconds
 
 //tap KeyE to eat orange
   document.addEventListener('keydown', (event) => {
@@ -89,6 +93,8 @@ Util.setPositionPixels(x, y, orange);
     }
   })
 
+  //swipe the keyboard row to move the player to left and right
+    //document.addEventListener()
   
 
   window.requestAnimationFrame(loop);
