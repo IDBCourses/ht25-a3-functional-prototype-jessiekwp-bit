c913ce93f006f853aeaf7f31afc8fbc7922d1034
jessiekwp-bit <jessiekwp@gmail.com>
2025-10-31 18:26:46 +0100

    falling orange

--- Template/script.js
+++ Template/script.js
@@ -7,7 +7,7 @@ import * as Util from "./util.js";
 
 // State variables are the parts of your program that change over time.
 let player = Util.thing;
-let orange0 = Util.createThing();
+let orange = Util.createThing();
 
 let px = (window.innerWidth*0.5);
 
@@ -28,9 +28,8 @@ let currentOrange = oranges[currentOrangeIndex];
 let x = window.innerWidth * currentOrange.x;
 let y = currentOrange.y;
 
-let fallSpeed = 2;
+let fallSpeed = 4.5;
 
-//let gameOver = false; 
 
 // Settings variables should contain all of the "fixed" parts of your programs
 
@@ -38,19 +37,29 @@ let fallSpeed = 2;
 // Code that runs over and over again
 function loop() {
 y += fallSpeed; 
-Util.setPositionPixels(x, y, orange0);
+Util.setPositionPixels(x, y, orange);
 
 
-//if (y > window.innerHeight){
-//  gameOver = true
-//} else {
-//  y +=0.5;
-//}
-
-//function gameOver(){
-//}
+if (y > window.innerHeight /*|| collision = true*/ ){
+  //alert ("Game Over!");
+  addOrange();
+} 
 
   window.requestAnimationFrame(loop);
+}
+
+function addOrange(){
+  // pick a random orange position from array
+ currentOrangeIndex = Math.floor(Math.random()* oranges.length);
+ currentOrange = oranges[currentOrangeIndex];
+
+ //reset orange position on top
+ x = window.innerWidth * currentOrange.x;
+ y = 0;
+}
+
+function eatOrange(){
+  Util.setColour(40, 100, 50, 0, orange);
 }
 
 
@@ -62,14 +71,23 @@ function setup() {
 Util.setColour(270, 100, 50, 0.5, player);
 Util.setRoundedness(0, player);
 Util.setSize(120,200, player);
-Util.setPositionPixels(px, window.innerHeight*0.6, player);
+Util.setPositionPixels(px, window.innerHeight * 0.85, player);
 
 //properties of orange0
-Util.setColour(40, 100, 50, 1, orange0);
-Util.setRoundedness(1, orange0);
-Util.setSize(100, 100, orange0);
-Util.setPositionPixels(x, y, orange0);
+Util.setColour(40, 100, 50, 1, orange);
+Util.setRoundedness(1, orange);
+Util.setSize(100, 100, orange);
+Util.setPositionPixels(x, y, orange);
 
+//setInterval(addOrange, 4000); // add an orange every 4 seconds
+
+//tap KeyE to eat orange
+  document.addEventListener('keydown', (event) => {
+    if(event.code === 'KeyE'){
+      console.log(`Key Down: Code ${event.code} | Key ${event.key}`);
+      eatOrange();
+    }
+  })
 
   
 
