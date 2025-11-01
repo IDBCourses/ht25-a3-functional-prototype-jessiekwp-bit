c177f427b3ad02b99b2b0bf14602557aabfafd0c
jessiekwp-bit <jessiekwp@gmail.com>
2025-11-01 17:08:31 +0100

    swipe to make player move

--- Template/script.js
+++ Template/script.js
@@ -13,14 +13,20 @@ import * as Util from "./util.js";
 // State variables are the parts of your program that change over time.
 let player = Util.thing;
 let orange = Util.createThing();
-let px = (window.innerWidth*0.5);
+
 let oranges = [];
 let initialFallSpeed = 2;
+
+let prevKey = null;
+let currKey = null;
+let timeoutID = null;
+let px = (window.innerWidth*0.5);
 
 
 // Settings variables should contain all of the "fixed" parts of your programs
 //const numRandomOranges = 5;
 const orangeXPos = [0.2, 0.9, 0.5, 0.3, 0.7, 0.1, 0.8, 0.4, 0.1, 0.6];
+const row = ['KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL'];
 
 for(let i=0; i < orangeXPos.length; i++){  //for loop 
   oranges.push({x: orangeXPos[i], y:0});
@@ -42,6 +48,8 @@ if (y > window.innerHeight /*|| collision = true*/ ){
   resetOrange();
 } 
 
+  Util.setPositionPixels(px, window.innerHeight *0.85, player)
+
   window.requestAnimationFrame(loop);
 }
 
@@ -59,6 +67,31 @@ function eatOrange(){
   Util.setColour(40, 100, 50, 0, orange);
 }
 
+function swipeDirection(){
+  let prevIndex = row.indexOf(prevKey);
+  let currIndex = row.indexOf(currKey);
+
+  console.log(`${prevIndex} -> ${currIndex}`);
+
+  if( currIndex < 0 || prevIndex < 0){
+    return 0;
+  } else {
+    let diffIndex = currIndex - prevIndex;
+
+    if(diffIndex > 1 || diffIndex < -1){
+      return 0;
+    } else {
+      return diffIndex;
+    }
+  }
+}
+
+function resetKeys(){
+  prevKey = null;
+  currKey = null;
+
+  console.log('Reset key presses.');
+}
 
 // Setup is run once, at the start of the program. It sets everything up for us!
 function setup() {
@@ -73,7 +106,7 @@ Util.setPositionPixels(px, window.innerHeight * 0.85, player);
 //properties of orange0
 Util.setColour(40, 100, 50, 1, orange);
 Util.setRoundedness(1, orange);
-Util.setSize(100, 100, orange);
+Util.setSize(80, 80, orange);
 Util.setPositionPixels(x, y, orange);
 
 
@@ -87,9 +120,26 @@ Util.setPositionPixels(x, y, orange);
 
   //swipe the keyboard row(KeyA-KeyL) to move the player left and right
     //document.addEventListener()
+  document.addEventListener('keydown', (event) => {
+    clearTimeout(timeoutID);
+
+    prevKey = currKey;
+    currKey = event.code;
+
+    console.log(`${prevKey} -> ${currKey}`);
+
+    let dir = swipeDirection();
+    px += dir*50;
+    Util.setPositionPixels(px, window.innerHeight *0.85, player)
+  })
+
+  document.addEventListener('keyup', (event) => {
+    timeoutID = setTimeout(resetKeys, 75);
+  })
   
 
   window.requestAnimationFrame(loop);
 }
 
 setup(); // Always remember to call setup()!
+
